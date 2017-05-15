333e50e82604ffbebbff6b9ae79f671f2947c47a
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2017-05-15 21:36:58 +1000

    Notes regarding dependencies and getting everything going

---
+++ installation_commands.sh
@@ -0,0 +1,17 @@
+
+
+# Install ANUGA as per docs
+
+# Install SWIMM as per Stephen's instructions
+
+git clone https://github.com/stoiver/Stormwater-Management-Model
+cd Stormwater-Management-Model
+
+# Switch Branch
+git checkout toolkitapi
+
+# Make shared library
+cd build/Linux/
+make libswmm5.so
+make swmm5
+sudo make install
